f70bea80ea1694a4587fb34122828f44ea345724
Luis Gustavo <dss.luisgustavo@gmail.com>
2023-01-13 11:03:20 -0300

    adicionando funções de Platform

---
+++ Domain/Platform.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Platform
+    {
+        public int Id;
+        public string Name;
+
+    }
+
+    public class PlatformDetails
+    {
+        public Platform platInfos;
+
+        public List<PublisherSales> publishers;
+        public List <Game> games;
+
+        public string Url;
+        public double Sales;
+
+    }
+
+    public class PlatformSales
+    {
+        public Platform platform;
+        public double Sales;
+        public string Url;
+    }
+}
