527ea3663533d412dfb80e1efba6bcef52843727
Luis Gustavo <dss.luisgustavo@gmail.com>
2023-01-14 18:22:42 -0300

    Adicionando a entidade Sales

---
+++ Domain/Sales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Sales
+    {
+        public double SalesValue;
+    }
+    public class Region_Sales
+    {
+        public Region region;
+        public string Url;
+        public double SalesValue;
+    }
+}
