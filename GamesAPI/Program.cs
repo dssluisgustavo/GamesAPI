6c48d4dd828b67d4846cf27eaad9110b15ff117b
Luis Gustavo <dss.luisgustavo@gmail.com>
2023-01-20 15:13:36 -0300

    Adicionando Validação do Token

--- GamesAPI/Program.cs
+++ GamesAPI/Program.cs
@@ -1,9 +1,12 @@
 using GamesAPI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using NPOI.SS.Formula.Functions;
 using Repository;
 using Repository.Interfaces;
 using Services;
 using Services.Interfaces;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +27,18 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ireliaVariosDedos")),
+        ValidIssuer = "GamesAPI",
+        ValidAudience = "Front"
+};
+});
 
 var app = builder.Build();
 
@@ -38,6 +53,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
